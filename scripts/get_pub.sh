a74cf27cf475bb497f16a93cdc1449d7d158eb29
constance <constance.beguier@gmail.com>
2019-10-03 21:24:48 +0200

    update: get public css/js, fix: mathjax

---
+++ scripts/get_pub.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+JSSOURCE="js/pub/.source.txt"
+CSSSOURCE="css/pub/.source.txt"
+
+while read -r line
+do
+	FILENAME=$(echo $line | awk '{print $1}')
+	URL=$(echo $line | awk '{print $2}')
+	curl "${URL}" -o "js/pub/${FILENAME}"
+done < "${JSSOURCE}"
+
+while read -r line
+do
+	FILENAME=$(echo $line | awk '{print $1}')
+	URL=$(echo $line | awk '{print $2}')
+	curl "${URL}" -o "css/pub/${FILENAME}"
+done < "${CSSSOURCE}"
