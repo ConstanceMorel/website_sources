95d26fe874b5497539c538b3544b1e5b5efba2d0
Nicolas Beguier <nicolas.beguier@adevinta.com>
2020-05-31 15:48:25 +0200

    Add README and remove local path in scripts/sync_ftp.sh

--- scripts/sync_ftp.sh
+++ scripts/sync_ftp.sh
@@ -3,7 +3,13 @@
 FTPUSER=$1
 FTPSERVER=$2
 
-LOCAL_DIRECTORY="/home/constance/Documents/website_sources/"
+LOCAL_DIRECTORY="${PWD}"
+if ! [ -d "${LOCAL_DIRECTORY}/.git" ]; then
+    echo "${LOCAL_DIRECTORY}/.git not found..."
+    echo "You may be in the wrong path"
+    exit 1
+fi
+
 REMOTE_DIRECTORY="www/"
 EXCLUDE_DIR_OPTS="--exclude-glob scripts/ --exclude-glob src/ --exclude-glob .git/ --exclude-glob .gitignore"
 
