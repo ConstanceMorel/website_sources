62faa2e8abba281e2e4786f420b3248095ceeaa5
nbeguier <nicolas_beguier@hotmail.com>
2020-11-28 14:04:34 +0100

    Upgrade JS/CSS & Fix sync_ftp.sh

--- scripts/sync_ftp.sh
+++ scripts/sync_ftp.sh
@@ -3,7 +3,7 @@
 FTPUSER=$1
 FTPSERVER=$2
 
-LOCAL_DIRECTORY="${PWD}"
+LOCAL_DIRECTORY="${PWD}/"
 if ! [ -d "${LOCAL_DIRECTORY}/.git" ]; then
     echo "${LOCAL_DIRECTORY}/.git not found..."
     echo "You may be in the wrong path"
@@ -11,6 +11,6 @@ if ! [ -d "${LOCAL_DIRECTORY}/.git" ]; then
 fi
 
 REMOTE_DIRECTORY="www/"
-EXCLUDE_DIR_OPTS="--exclude-glob scripts/ --exclude-glob src/ --exclude-glob .git/ --exclude-glob .gitignore"
+EXCLUDE_DIR_OPTS="--exclude-glob scripts/ --exclude-glob src/ --exclude-glob .git/ --exclude-glob .gitignore --exclude-glob .DS_Store"
 
 lftp -e "mirror --delete --reverse ${EXCLUDE_DIR_OPTS} ${LOCAL_DIRECTORY} ${REMOTE_DIRECTORY}; exit" -u "${FTPUSER}" "${FTPSERVER}"
